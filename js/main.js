dd1fdccd576784316687f84651af3d96b7300375
felipecarvalho <felipe.carvalho_2016@outlook.com>
2024-01-06 18:58:00 -0300

    adicionando JS

--- js/main.js
+++ js/main.js
@@ -4,14 +4,39 @@ const btnLongo = document.querySelector('.longo');
 
 const circulo = document.getElementById('cor-circulo')
 
-btnLongo.addEventListener('click', ()=>{
-    circulo.setAttribute('data-contexto', 'longo');
+
+
+btnFoco.addEventListener('click', ()=>{
+    circulo.setAttribute('data-contexto', 'foco');
+
+    if(btnShort.classList.contains('active') || btnLongo.classList.contains('active')){
+        btnShort.classList.remove('active');
+        btnLongo.classList.remove('active');
+    }
+
+    btnFoco.classList.add('active')
 })
 
 btnShort.addEventListener('click', ()=>{
     circulo.setAttribute('data-contexto', 'short');
+
+    if(btnFoco.classList.contains('active') || btnLongo.classList.contains('active')){
+        btnFoco.classList.remove('active');
+        btnLongo.classList.remove('active');
+    }
+
+    btnShort.classList.add('active')
 })
 
-btnFoco.addEventListener('click', ()=>{
-    circulo.setAttribute('data-contexto', 'foco');
+
+
+btnLongo.addEventListener('click', ()=>{
+    circulo.setAttribute('data-contexto', 'longo');
+
+    if(btnFoco.classList.contains('active') || btnShort.classList.contains('active')){
+        btnFoco.classList.remove('active');
+        btnShort.classList.remove('active');
+    }
+
+    btnLongo.classList.add('active')
 })
